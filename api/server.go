bd736e0ba0bf3251cb7fcd5953ada59a948c877a
Hirochon <heacet43@gmail.com>
2021-07-11 21:04:59 +0900

    [add] DI！！！！

--- api/server.go
+++ api/server.go
@@ -1,25 +1,27 @@
 package main
 
 import (
+	"api/handler"
 	"api/infra"
 	"api/middleware"
+	"api/usecase"
 	"fmt"
 	"net/http"
 )
 
 var addr = ":8080"
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
-}
-
 func main() {
 	myClient := infra.Init()
 	defer myClient.Close()
 	fmt.Println(myClient)
 
+	statusRepository := infra.NewStatusRepository(myClient)
+	statusUsecase := usecase.NewStatusUsecase(statusRepository)
+	taskHandler := handler.NewStatusHandler(statusUsecase)
+
 	router := http.NewServeMux()
-	router.HandleFunc("/", handler)
+	router.HandleFunc("/", taskHandler.POST())
 	fmt.Printf("[START] server. port: %s\n", addr)
 	if err := http.ListenAndServe(addr, middleware.Log(router)); err != nil {
 		panic(fmt.Errorf("[FAILED] start sever. err: %v", err))
